d4a731cf5df491a6f352c5a04e75739d32e7c22c
Benjamin <bengdennis23@gmail.com>
2024-04-04 18:21:29 -0600

    push1

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,7 +15,8 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+const addFive = num => num + 5;
+addFive();
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
